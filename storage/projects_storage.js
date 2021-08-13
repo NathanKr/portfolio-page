fc670313cdec0e5303aa8840b0ea466775755905
Nathan Krasney <natankrasney@gmail.com>
2021-08-13 12:03:35 +0300

    png -> jpg

--- storage/projects_storage.js
+++ storage/projects_storage.js
@@ -12,11 +12,11 @@ const projects = [
     "Marketing : Personal Web Site",
     ["React", "Node", "Express", "Digital Ocean", "Nginx", "SSL"],
     "Static SPA web site based on React ",
-    "nathan_krasney_com.png",
+    "nathan_krasney_com.jpg",
     [
-      "nathan_krasney_com_slider_1.png",
-      "nathan_krasney_com_slider_2.png",
-      "nathan_krasney_com_slider_3.png",
+      "nathan_krasney_com_slider_1.jpg",
+      "nathan_krasney_com_slider_2.jpg",
+      "nathan_krasney_com_slider_3.jpg",
     ],
     new MoreInfo("https://nathankrasney.com/", true, constants.MODE_SITE)
   ),
@@ -34,8 +34,8 @@ const projects = [
       "Web API",
     ],
     "Cash On Tab is a computerize and compact Point Of Sale with an admin system on the cloud. The first version was implemented using C# on windows mobile device and the next version was implemented using java on an android device",
-    "cash-on-tab.png",
-    ["cash_on_tab_slider_1.png"],
+    "cash-on-tab.jpg",
+    ["cash_on_tab_slider_1.jpg"],
     new MoreInfo(
       "https://www.autosoft.co.il/%d7%9e%d7%95%d7%a6%d7%a8%d7%99-%d7%90%d7%95%d7%98%d7%95%d7%a1%d7%95%d7%a4%d7%98/cashontab/",
       true,
@@ -57,8 +57,8 @@ const projects = [
       "UML",
     ],
     "UltraShape® is the first and only FDA-cleared, non-invasive body-shaping procedure to use focused, pulsed ultrasound energy to selectively destroy fat cells. Responsible for the development of software to hardware interfaces and software tools for the transducers production lines",
-    "ultrashape.png",
-    ["ultrashape_slider_1.png", "ultrashape_slider_2.png"],
+    "ultrashape.jpg",
+    ["ultrashape_slider_1.jpg", "ultrashape_slider_2.jpg"],
     new MoreInfo(
       "https://candelamedical.com/na/patient/product/ultrashape",
       true,
@@ -80,12 +80,12 @@ const projects = [
       "ASP.net",
     ],
     "Learn Online Courses is a web platform that help teachers to create online courses and help students to learn courses online anytime anywhere.",
-    "learn_online_courses.png",
+    "learn_online_courses.jpg",
     [
-      "learn_online_courses_slider_1.png",
-      "learn_online_courses_slider_2.png",
-      "learn_online_courses_slider_3.png",
-      "learn_online_courses_slider_4.png",
+      "learn_online_courses_slider_1.jpg",
+      "learn_online_courses_slider_2.jpg",
+      "learn_online_courses_slider_3.jpg",
+      "learn_online_courses_slider_4.jpg",
     ],
     new MoreInfo("", false) //todo change to video
   ),
@@ -94,8 +94,8 @@ const projects = [
     "Communication",
     ["React", "Hooks", "Firebase", "Semantic UI React"],
     "The client is implemented using React hooks. The server is implemented using Firebase to enable chat, authentication and messages storage",
-    "chat_app.png",
-    ["chat_app_slider_1.png", "chat_app_slider_2.png"],
+    "chat_app.jpg",
+    ["chat_app_slider_1.jpg", "chat_app_slider_2.jpg"],
     new MoreInfo(
       "https://www.youtube.com/watch?v=3bzcJonDPc8",
       true,
@@ -115,13 +115,13 @@ const projects = [
       "SQL server",
     ],
     "Share location between parents and kids , friends , workers and boss. The client locations is saved on the server data base and can be shared by followers.",
-    "follow_my_route.png",
+    "follow_my_route.jpg",
     [
-      "follow_my_route_slider_1.png",
-      "follow_my_route_slider_2.png",
-      "follow_my_route_slider_3.png",
-      "follow_my_route_slider_4.png",
-      "follow_my_route_slider_5.png",
+      "follow_my_route_slider_1.jpg",
+      "follow_my_route_slider_2.jpg",
+      "follow_my_route_slider_3.jpg",
+      "follow_my_route_slider_4.jpg",
+      "follow_my_route_slider_5.jpg",
     ],
     new MoreInfo(
       "https://www.youtube.com/watch?v=DKoRnBX9m1A",
@@ -145,7 +145,7 @@ const projects = [
     ],
     "Wind Notifier allows wind and kite surfers to receive email notifications when the wind is strong. It eliminates the need to manually poll wind stations for strong winds",
     "wind_notifier.jpg",
-    ["wind_notifier_slider_1.png", "wind_notifier_slider_2.png"],
+    ["wind_notifier_slider_1.jpg", "wind_notifier_slider_2.jpg"],
     new MoreInfo("", false)
   ),
   new ProjectLogic(
@@ -162,8 +162,8 @@ const projects = [
       "GoDaddy",
     ],
     "The FlashAd system implements a new advertising approach for smartphones in which the Advertiser (company/store) itself can launch a campaign in Real-Time based on a specific rule and using an admin panel",
-    "flash_ad.png",
-    ["flash_ad_slider_1.png", "flash_ad_slider_2.png", "flash_ad_slider_3.png"],
+    "flash_ad.jpg",
+    ["flash_ad_slider_1.jpg", "flash_ad_slider_2.jpg", "flash_ad_slider_3.jpg"],
     new MoreInfo("", false)
   ),
   new ProjectLogic(
@@ -171,11 +171,11 @@ const projects = [
     "Education : Math Game",
     ["Android", "Java", "Open GL"],
     "Jumper Start Plus is : educational , fun ,simple , math game for kids age 5 - 10. Play the game , have fun , collect coins and learn math addition operation along the way. It is part of 4 educational math games series",
-    "jumper_start_plus.png",
+    "jumper_start_plus.jpg",
     [
-      "jumper_start_plus_slider_1.png",
-      "jumper_start_plus_slider_2.png",
-      "jumper_start_plus_slider_3.png",
+      "jumper_start_plus_slider_1.jpg",
+      "jumper_start_plus_slider_2.jpg",
+      "jumper_start_plus_slider_3.jpg",
     ],
     new MoreInfo(
       "https://www.youtube.com/watch?v=Dem_La1dKp4&t=6s",
